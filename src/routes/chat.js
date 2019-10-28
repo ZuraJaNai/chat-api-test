bb2ff17299e306d3a7ffe26882046879c3931d16
zurajanai <nckkvtn@gmail.com>
2019-10-28 20:14:47 +0200

    endpoints for chat route

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -6,12 +6,22 @@ const Chat = require('../models/chat');
 const User = require('../models/user');
 
 // @route GET /chat
-// @desc get list of chats for current user
+// @desc Get list of chats for current user
 // @access Private
 router.get('/', (req, res) => {
-  const currentUser = User.find({ _id: req.user.id });
-  Chat.find({ users: currentUser })
-    .then(results => res.status(200).json(JSON.stringify(results)))
+  Chat.find({ users: req.user.id })
+    .then(results => res.status(200).json(results))
+    .catch(err => res.status(500).json(err));
+});
+
+// @route POST /chat/(userId)
+// @desc create new chat with another user
+// @access Private
+router.post('/:userId', (req, res) => {
+  Chat.create({
+    users: [req.user.id, req.params.userId],
+  })
+    .then(response => res.status(201).json(response))
     .catch(err => res.status(500).json(err));
 });
 
